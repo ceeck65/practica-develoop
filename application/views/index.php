1c897b89880b90851806c976425c7f904c523dff
Mario Fernandez <alespcs@gmail.com>
2016-10-26 04:07:49 -0430

    Modificaciones maquetado

--- application/views/index.php
+++ application/views/index.php
@@ -37,22 +37,85 @@
         <li><a href="#">Empresas</a></li>
         <li class="active">Registro de empresas</li>
     </ol>
+    <div class="gradient"></div>
+    <section>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="col-md-6 enterprise">
+                    <h1>REGISTRO DE EMPRESAS</h1>
+                </div>
+                <div class="col-md-6">
+                    <button class="btn btn-primary save-enterprise pull-right">Guardar cambios</button>
+                </div>
+            </div>
+            <hr style="width: 95%; color: #d2d2d2; height: 4px; margin-top: 20px !important;" />
+            <div class="col-md-12">
+                <button class="btn btn-primary">Guardar</button>
+            </div>
+        </div>
+    </section>
 
-
-
-
-
-
-    <!-- Main component for a primary marketing message or call to action -->
-    <div class="jumbotron">
-        <h1>Navbar example</h1>
-        <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-        <p>
-            <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
-        </p>
-    </div>
-
+    <!-- footer -->
+    <div class="gradient"></div>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="col-md-3 footer">
+                        <h3>Empresas</h3>
+                        <ul>
+                            <li>Consequat</li>
+                            <li>Nunc tempor</li>
+                            <li>Ante quis libero</li>
+                            <li>Fermentum</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 footer">
+                        <h3>Servicios</h3>
+                        <ul>
+                            <li>Consequat</li>
+                            <li>Nunc tempor</li>
+                            <li>Ante quis libero</li>
+                            <li>Fermentum</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 footer">
+                        <h3>Valora un servicio</h3>
+                        <ul><li>Sed congue suscipit</li>
+                            <li>Consequat Nunc</li>
+                            <li>tempor  Ante</li>
+                            <li>quis libero  F</li>
+                            <li>Consequat</li>
+                            <li>Nunc tempor</li>
+                            <li>Ante quis libero</li>
+                            <li>Fermentum</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 footer">
+                        <h3>Contacto</h3>
+                        <ul>
+                            <li>Consequat</li>
+                            <li>Nunc tempor</li>
+                            <li>Ante quis libero</li>
+                            <li>Fermentum</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-12 footer">
+                       <p>
+                           an inclined part of a road or railway; a slope.
+                           fail-safe brakes for use on steep gradients
+                           sinónimos: slope, incline, hill, rise, ramp, bank, declivity, grade
+                           an increase or decrease in the magnitude of a property (e.g., temperature, pressure, or concentration) observed in passing from one point or moment to another.
+                       </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- /footer -->
 </div> <!-- /container -->
+
+
 
  <!-- Bootstrap core JavaScript
     ================================================== -->
